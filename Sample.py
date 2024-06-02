e686edc664ac0a263116c1501e2d410c1bf4aa21
Maryam Farooq <maryamfarooq_91@yahoo.com>
2024-06-02 01:13:09 -0400

    Initial commit

---
+++ Sample.py
@@ -0,0 +1,12 @@
+print('Hello')
+
+numbers = ["8", "2", "7", "3", "10"]
+
+for x in numbers:
+    for j in numbers:
+        if x == 10:
+            print(x)
+            print('Sum is 10')
+        else:
+            print(x)
+            print('Sum is not 10')
