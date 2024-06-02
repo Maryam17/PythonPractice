22a62df2db35ed53f26e0c65724c712f7cd7cb92
Maryam Farooq <maryamfarooq_91@yahoo.com>
2024-06-02 01:16:02 -0400

    Change 10 to 20

--- Sample.py
+++ Sample.py
@@ -4,9 +4,9 @@ numbers = ["8", "2", "7", "3", "10"]
 
 for x in numbers:
     for j in numbers:
-        if x == 10:
+        if x == 20:
             print(x)
-            print('Sum is 10')
+            print('Sum is 20')
         else:
             print(x)
-            print('Sum is not 10')
+            print('Sum is not 20')
